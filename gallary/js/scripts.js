42ba31c0abf68d55ec35bc97672ab858db873009
AMassick <adam.massick@gmail.com>
2014-03-02 19:10:25 -0800

    Revert "Revert "image gallary""
    This reverts commit 42bd45a06069af1eb1fbb3ab806e6f5f5ea1e7c1.

---
+++ gallary/js/scripts.js
@@ -0,0 +1,11 @@
+// Make Foundation Go!
+$(document).foundation();
+
+// Your Awesome Scripts!
+$(document).ready(function(){
+
+	$('#gallary').least();
+
+	console.log('Page Loaded. Lets Do this!');
+
+}); 
